dfcda26e3a6a0ed6fca0878b804c44da6d23cf7d
Blake Mischley <98132827+BMischley@users.noreply.github.com>
2024-04-24 10:39:36 -0400

    Update index.tsx to typescript

--- packages/app/app/components/Dashboard/GenresTab/index.tsx
+++ packages/app/app/components/Dashboard/GenresTab/index.tsx
@@ -2,55 +2,55 @@ import { SmoothImage } from '@nuclear/ui';
 import React from 'react';
 import { Tab } from 'semantic-ui-react';
 import _ from 'lodash';
-
 import genreToIcon from './mapGenres';
 import styles from './styles.scss';
-import { LastfmTopTag } from '@nuclear/core/src/rest/Lastfm.types';
-import { useHistory } from 'react-router-dom';
 
-const bannedGenres = [
-  'seen live'
-];
-
-type GenresTabProps = {
-  genres: LastfmTopTag[]
+interface Genre {
+  name: string;
 }
 
-const GenresTab: React.FC<GenresTabProps> = ({ genres }) => {
-  const history = useHistory();
+interface GenresTabProps {
+  genres?: Genre[];
+  history: {
+    push: (path: string) => void;
+  };
+}
 
-  function onGenreClick(genreName: string) {
-    history.push(`/tag/${genreName}`);
+const bannedGenres = ['seen live'];
+
+class GenresTab extends React.Component<GenresTabProps> {
+  constructor(props: GenresTabProps) {
+    super(props);
   }
 
-  return (
-    <Tab.Pane attached={false}>
-      <div className={styles.genre_tab_container}>
-        {
-          _.filter(genres, genre => !_.includes(bannedGenres, genre.name)).map((tag, i) => {
-            return (
-              <div
-                className={styles.genre_container}
-                key={i}
-                onClick={() => onGenreClick(tag.name)}
-              >
-                <div className={styles.genre_overlay}>
-                  <SmoothImage src={`https://picsum.photos/256?random=${i}`} />
-                </div>
-                <div className={styles.genre_name}>
-                  <div
-                    className={styles.svg_icon}
-                    dangerouslySetInnerHTML={{ __html: genreToIcon(tag.name) }}
-                  />
-                  { tag.name }
-                </div>
-              </div>
-            );
-          })
-        }
-      </div>
-    </Tab.Pane>
-  );
-};
+  onGenreClick(genreName: string) {
+    this.props.history.push('/tag/' + genreName);
+  }
+
+  render() {
+    const { genres } = this.props;
+    return (
+      <Tab.Pane attached={false}>
+        <div className={styles.genre_tab_container}>
+          {typeof genres !== 'undefined'
+            ? _.filter(genres, genre => !_.includes(bannedGenres, genre.name)).map((tag, i) => {
+                return (
+                  <div className={styles.genre_container} key={i} onClick={() => this.onGenreClick(tag.name)}>
+                    <div className={styles.genre_overlay}>
+                      <SmoothImage src={'https://picsum.photos/256?random=' + i} />
+                    </div>
+                    <div className={styles.genre_name}>
+                      <div className={styles.svg_icon} dangerouslySetInnerHTML={{ __html: genreToIcon(tag.name) }} />
+                      {tag.name}
+                    </div>
+                  </div>
+                );
+              })
+            : null}
+        </div>
+      </Tab.Pane>
+    );
+  }
+}
 
 export default GenresTab;
